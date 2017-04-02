a42380d77c48668f000ffaeca93f51162c05414f
Ruellia Zhang <syncyes@gmail.com>
2017-04-02 09:47:38 -0700

    ui with mock data

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import {Router, Route, hashHistory} from 'react-router';
+import App from './components/App';
 import Voting from './components/Voting';
+import Results from './components/Results';
 
-const pair = ['Trainspotting', '28 Days Later'];
+const routes = <Route component={App}>
+  <Route path="/" component={Voting} />
+  <Route path="/results" component={Results} />
+</Route>;
 
 ReactDOM.render(
-  <Voting pair={pair} hasVoted="Trainspotting"/>,
+  <Router history={hashHistory}>{routes}</Router>,
   document.getElementById('root')
 );
