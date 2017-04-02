2bbaab46e52513ca47e52e6dea49974970cf28af
Ruellia Zhang <syncyes@gmail.com>
2017-04-02 09:55:28 -0700

    redux store that is connected to pure components

--- src/components/Voting.test.js
+++ src/components/Voting.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {renderIntoDocument, scryRenderedDOMComponentsWithTag, Simulate} from 'react-addons-test-utils';
-import Voting from '../../src/components/Voting';
+import {Voting} from '../../src/components/Voting';
 import {List} from 'immutable';
 import {expect} from 'chai';
 
